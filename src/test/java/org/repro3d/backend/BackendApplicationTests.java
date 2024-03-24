33cef888201b68a9ee7ccfd5f27d2e8614c6e553
LostPersona <artdvmail@gmail.com>
2024-03-24 13:41:35 +0100

    feat: Spring project initialized RP-18
    This commit adds the initialized Spring project core with its basic structure.

---
+++ src/test/java/org/repro3d/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.repro3d.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
