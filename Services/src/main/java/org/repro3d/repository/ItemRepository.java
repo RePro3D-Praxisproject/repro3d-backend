6e2847e37f3a4f7b9bc21596e6d34a187925f41d
LostPersona <artdvmail@gmail.com>
2024-04-02 19:06:15 +0200

    docs: Adds comment to all classes in Service module

--- Services/src/main/java/org/repro3d/repository/ItemRepository.java
+++ Services/src/main/java/org/repro3d/repository/ItemRepository.java
@@ -6,8 +6,26 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * Repository interface for {@link Item} entities.
+ * This interface extends JpaRepository, providing CRUD operations and additional methods to interact with the Item data.
+ */
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    /**
+     * Finds a list of items by their name.
+     *
+     * @param name The name of the items to search for.
+     * @return A list of {@link Item} objects that match the specified name. If no items are found, returns an empty list.
+     */
     List<Item> findByName(String name);
+
+    /**
+     * Finds a list of items created by a specific user.
+     *
+     * @param creatorUserId The user ID of the item's creator.
+     * @return A list of {@link Item} objects created by the specified user. If no items are found, returns an empty list.
+     */
     List<Item> findByCreatorUserId(Long creatorUserId);
 }
--- Services/src/main/java/org/repro3d/service/ItemService.java
+++ Services/src/main/java/org/repro3d/service/ItemService.java
@@ -10,21 +10,43 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Service class for handling operations related to {@link Item} entities.
+ * This service encapsulates the business logic for creating, retrieving, updating, and deleting items,
+ * interacting with the {@link ItemRepository} to perform these operations.
+ */
 @Service
 public class ItemService {
 
     private final ItemRepository itemRepository;
 
+    /**
+     * Constructs an item service with the necessary item repository.
+     *
+     * @param itemRepository The repository used for item data operations.
+     */
     @Autowired
     public ItemService(ItemRepository itemRepository) {
         this.itemRepository = itemRepository;
     }
 
+    /**
+     * Creates and saves a new item in the repository.
+     *
+     * @param item The item to be created and saved.
+     * @return A {@link ResponseEntity} containing an {@link ApiResponse} with the result of the create operation.
+     */
     public ResponseEntity<ApiResponse> createItem(Item item) {
         Item savedItem = itemRepository.save(item);
         return ResponseEntity.ok(new ApiResponse(true, "Item created successfully", savedItem));
     }
 
+    /**
+     * Retrieves an item by its ID.
+     *
+     * @param id The ID of the item to retrieve.
+     * @return A {@link ResponseEntity} containing an {@link ApiResponse} with the item if found, or an error message otherwise.
+     */
     public ResponseEntity<ApiResponse> getItemById(Long id) {
         Optional<Item> item = itemRepository.findById(id);
         if (item.isPresent()) {
@@ -34,6 +56,12 @@ public class ItemService {
         }
     }
 
+    /**
+     * Retrieves a list of items matching a given name.
+     *
+     * @param name The name to search for among items.
+     * @return A {@link ResponseEntity} containing an {@link ApiResponse} with the list of found items or an error message if none found.
+     */
     public ResponseEntity<ApiResponse> getByName(String name) {
         List<Item> items = itemRepository.findByName(name);
         if (!items.isEmpty()) {
@@ -43,6 +71,12 @@ public class ItemService {
         }
     }
 
+    /**
+     * Retrieves a list of items created by a specific user.
+     *
+     * @param creatorUserId The ID of the user whose items to retrieve.
+     * @return A {@link ResponseEntity} containing an {@link ApiResponse} with the list of items or an error message if none found.
+     */
     public ResponseEntity<ApiResponse> getByCreatorUserId(Long creatorUserId) {
         List<Item> items = itemRepository.findByCreatorUserId(creatorUserId);
         if (!items.isEmpty()) {
@@ -52,11 +86,23 @@ public class ItemService {
         }
     }
 
+    /**
+     * Retrieves all items in the repository.
+     *
+     * @return A {@link ResponseEntity} containing an {@link ApiResponse} with the list of all items.
+     */
     public ResponseEntity<ApiResponse> getAllItems() {
         Iterable<Item> items = itemRepository.findAll();
         return ResponseEntity.ok(new ApiResponse(true, "Items retrieved successfully", items));
     }
 
+    /**
+     * Updates an existing item identified by its ID with new details.
+     *
+     * @param id The ID of the item to update.
+     * @param itemDetails The new details for the item.
+     * @return A {@link ResponseEntity} containing an {@link ApiResponse} with the updated item, or an error message if the item was not found.
+     */
     public ResponseEntity<ApiResponse> updateItem(Long id, Item itemDetails) {
         return itemRepository.findById(id)
                 .map(item -> {
@@ -71,6 +117,12 @@ public class ItemService {
                 }).orElseGet(() -> ResponseEntity.ok(new ApiResponse(false, "Item not found for ID: " + id, null)));
     }
 
+    /**
+     * Deletes an item from the repository identified by its ID.
+     *
+     * @param id The ID of the item to delete.
+     * @return A {@link ResponseEntity} containing an {@link ApiResponse} indicating the result of the delete operation.
+     */
     public ResponseEntity<ApiResponse> deleteItem(Long id) {
         if (itemRepository.existsById(id)) {
             itemRepository.deleteById(id);
