c60b116e6817d6ffc27b84841b0483f68baf230b
LostPersona <artdvmail@gmail.com>
2024-04-28 18:51:36 +0200

    refactor: Use /api in Controller classes

--- BillingService/src/main/java/org/repro3d/controller/RedeemCodeController.java
+++ BillingService/src/main/java/org/repro3d/controller/RedeemCodeController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/redeem-code")
+@RequestMapping("/api/redeem-code")
 public class RedeemCodeController {
 
     private final RedeemCodeService redeemCodeService;
