b0d3b54a65fc6ce784f5db19fdd84596c73789c9
LostPersona <artdvmail@gmail.com>
2024-04-28 13:51:56 +0200

    feat: Add Billing related changesto the OrderService

--- OrderService/src/test/java/org/repro3d/service/OrderServiceTest.java
+++ OrderService/src/test/java/org/repro3d/service/OrderServiceTest.java
@@ -3,23 +3,21 @@ package org.repro3d.service;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.repro3d.model.Order;
+import org.repro3d.model.RedeemCode;
+import org.repro3d.model.User;
+import org.repro3d.repository.OrderRepository;
+import org.repro3d.repository.RedeemCodeRepository;
+import org.repro3d.repository.UserRepository;
+import org.repro3d.utils.ApiResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.http.ResponseEntity;
 
-import repro3d.model.Order;
-import repro3d.model.User;
-import repro3d.repository.OrderRepository;
-import repro3d.repository.UserRepository;
-import repro3d.service.OrderService;
-import repro3d.utils.ApiResponse;
-
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -33,39 +31,69 @@ public class OrderServiceTest {
     @Mock
     private UserRepository userRepository;
 
+    @Mock
+    private RedeemCodeRepository redeemCodeRepository;
+
     @InjectMocks
     private OrderService orderService;
 
     private Order order;
     private User user;
+    private RedeemCode redeemCode;
 
     @BeforeEach
     void setUp() {
-        user = new User();
-        user.setUserId(1L); // Assuming User class has this method
-
-        order = new Order();
-        order.setOrder_id(1L);
-        order.setUser(user);
-        order.setRcId(2L);
-
-        lenient().when(userRepository.existsById(user.getUserId())).thenReturn(true);
+        user = new User(1L, "user@example.com", "1234 Street", null);
+        redeemCode = new RedeemCode(2L, "SOME_CODE", false);
+        order = new Order(1L, null, user, redeemCode);
     }
-
 
     @Test
     void createOrderSuccessfully() {
+        when(userRepository.existsById(user.getUserId())).thenReturn(true);
+        when(redeemCodeRepository.existsById(redeemCode.getRc_id())).thenReturn(true);
+        when(redeemCodeRepository.findById(redeemCode.getRc_id())).thenReturn(Optional.ofNullable(redeemCode));
+
         when(orderRepository.save(any(Order.class))).thenReturn(order);
+
         ResponseEntity<ApiResponse> response = orderService.createOrder(order);
+        System.out.println(response.getBody().getMessage());
         assertEquals(200, response.getStatusCodeValue());
         assertTrue(response.getBody().isSuccess());
         assertEquals(order, response.getBody().getData());
+
     }
 
     @Test
     void createOrderFailureUserNotFound() {
         when(userRepository.existsById(anyLong())).thenReturn(false);
+
         ResponseEntity<ApiResponse> response = orderService.createOrder(order);
+
+        assertEquals(400, response.getStatusCodeValue());
+        assertFalse(response.getBody().isSuccess());
+    }
+
+    @Test
+    void createOrderFailureRedeemCodeNotFound() {
+        when(userRepository.existsById(user.getUserId())).thenReturn(true);
+        when(redeemCodeRepository.existsById(redeemCode.getRc_id())).thenReturn(false);
+
+        ResponseEntity<ApiResponse> response = orderService.createOrder(order);
+
+        assertEquals(400, response.getStatusCodeValue());
+        assertFalse(response.getBody().isSuccess());
+    }
+
+    @Test
+    void createOrderFailureRedeemCodeUsed() {
+        redeemCode.setUsed(true);
+        when(userRepository.existsById(user.getUserId())).thenReturn(true);
+        when(redeemCodeRepository.existsById(redeemCode.getRc_id())).thenReturn(true);
+        lenient().when(redeemCodeRepository.findById(redeemCode.getRc_id())).thenReturn(Optional.of(redeemCode));
+
+        ResponseEntity<ApiResponse> response = orderService.createOrder(order);
+
         assertEquals(400, response.getStatusCodeValue());
         assertFalse(response.getBody().isSuccess());
     }
@@ -73,7 +101,9 @@ public class OrderServiceTest {
     @Test
     void getOrderByIdFound() {
         when(orderRepository.findById(anyLong())).thenReturn(Optional.of(order));
+
         ResponseEntity<ApiResponse> response = orderService.getOrderById(1L);
+
         assertEquals(200, response.getStatusCodeValue());
         assertTrue(response.getBody().isSuccess());
         assertEquals(order, response.getBody().getData());
@@ -82,35 +112,23 @@ public class OrderServiceTest {
     @Test
     void getOrderByIdNotFound() {
         when(orderRepository.findById(anyLong())).thenReturn(Optional.empty());
+
         ResponseEntity<ApiResponse> response = orderService.getOrderById(1L);
+
         assertEquals(200, response.getStatusCodeValue());
         assertFalse(response.getBody().isSuccess());
     }
 
     @Test
-    void getAllOrdersWithResult() {
-        when(orderRepository.findAll()).thenReturn(Arrays.asList(order));
-        ResponseEntity<ApiResponse> response = orderService.getAllOrders();
-        assertEquals(200, response.getStatusCodeValue());
-        assertTrue(response.getBody().isSuccess());
-        assertNotNull(response.getBody().getData());
-        List<Order> orders = (List<Order>) response.getBody().getData();
-        assertFalse(orders.isEmpty());
-    }
-
-    @Test
-    void getAllOrdersEmpty() {
-        when(orderRepository.findAll()).thenReturn(Arrays.asList());
-        ResponseEntity<ApiResponse> response = orderService.getAllOrders();
-        assertEquals(200, response.getStatusCodeValue());
-        assertFalse(response.getBody().isSuccess());
-    }
-
-    @Test
-    void updateOrderFound() {
-        when(orderRepository.findById(anyLong())).thenReturn(Optional.of(order));
-        when(orderRepository.save(any(Order.class))).thenReturn(order);
+    void updateOrderFoundAndValid() {
+        lenient().when(orderRepository.findById((order.getOrder_id()))).thenReturn(Optional.of(order));
+        lenient().when(userRepository.existsById(user.getUserId())).thenReturn(true);
+        lenient().when(redeemCodeRepository.existsById(redeemCode.getRc_id())).thenReturn(true);
+        lenient().when(redeemCodeRepository.findById(redeemCode.getRc_id())).thenReturn(Optional.of(redeemCode));
+        lenient().when(orderRepository.findById(anyLong())).thenReturn(Optional.of(order));
+        lenient().when(orderRepository.save(any(Order.class))).thenReturn(order);
         ResponseEntity<ApiResponse> response = orderService.updateOrder(1L, order);
+
         assertEquals(200, response.getStatusCodeValue());
         assertTrue(response.getBody().isSuccess());
         assertEquals(order, response.getBody().getData());
@@ -118,9 +136,22 @@ public class OrderServiceTest {
 
     @Test
     void updateOrderNotFound() {
-        when(orderRepository.findById(anyLong())).thenReturn(Optional.empty());
         ResponseEntity<ApiResponse> response = orderService.updateOrder(1L, order);
+
         assertEquals(200, response.getStatusCodeValue());
+        assertFalse(response.getBody().isSuccess());
+    }
+
+    @Test
+    void updateOrderFailureRedeemCodeUsed() {
+        redeemCode.setUsed(true);
+        lenient().when(orderRepository.findById((order.getOrder_id()))).thenReturn(Optional.of(order));
+        lenient().when(userRepository.existsById(user.getUserId())).thenReturn(true);
+        lenient().when(redeemCodeRepository.existsById(redeemCode.getRc_id())).thenReturn(true);
+        when(redeemCodeRepository.findById(redeemCode.getRc_id())).thenReturn(Optional.of(redeemCode));
+        ResponseEntity<ApiResponse> response = orderService.updateOrder(1L, order);
+
+        assertEquals(400, response.getStatusCodeValue());
         assertFalse(response.getBody().isSuccess());
     }
 
@@ -128,7 +159,9 @@ public class OrderServiceTest {
     void deleteOrderFound() {
         when(orderRepository.existsById(anyLong())).thenReturn(true);
         doNothing().when(orderRepository).deleteById(anyLong());
+
         ResponseEntity<ApiResponse> response = orderService.deleteOrder(1L);
+
         assertEquals(200, response.getStatusCodeValue());
         assertTrue(response.getBody().isSuccess());
     }
@@ -136,7 +169,9 @@ public class OrderServiceTest {
     @Test
     void deleteOrderNotFound() {
         when(orderRepository.existsById(anyLong())).thenReturn(false);
+
         ResponseEntity<ApiResponse> response = orderService.deleteOrder(1L);
+
         assertEquals(200, response.getStatusCodeValue());
         assertFalse(response.getBody().isSuccess());
     }
