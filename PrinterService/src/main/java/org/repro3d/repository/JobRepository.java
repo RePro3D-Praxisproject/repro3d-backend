06e782369383088558ae490830d19a72794e26c6
LostPersona <artdvmail@gmail.com>
2024-05-25 15:06:00 +0200

    feat: Add finding status by job id to job repo

--- PrinterService/src/main/java/org/repro3d/repository/JobRepository.java
+++ PrinterService/src/main/java/org/repro3d/repository/JobRepository.java
@@ -1,8 +1,11 @@
 package org.repro3d.repository;
 
+import org.repro3d.model.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.repro3d.model.Job;
+
+import java.util.List;
 
 /**
  * Repository interface for {@link Job} entities.
@@ -12,4 +15,6 @@ import org.repro3d.model.Job;
  */
 @Repository
 public interface JobRepository extends JpaRepository<Job, Long> {
+    List<Job> findByStatusOrderByJobIdAsc(Status status);
 }
+
